79608becf517047c04c14338e42c2ab8e5e4964c
Deadman407 <dead.407.man@gmail.com>
2021-08-20 13:56:59 -0700

    Photography Website

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const pug = require("pug");
+
+const port = process.env.PORT || 3000;
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+// PUB SPECIFIC STUFF
+app.set('view engine', 'pug'); // Set the template engine as pug
+app.set('views', path.join(__dirname, "views")); // Set the views directory
+
+//ENDPOINTS
+app.get('/', (req, res) => {
+    const params = {}
+    res.status(200).render('home.pug', params);
+})
+
+app.get('/about', (req, res) => {
+    const params = {}
+    res.status(200).render('about.pug', params);
+})
+
+app.get('/contact', (req, res) => {
+    const params = {}
+    res.status(200).render('contact.pug', params);
+})
+
+// START THE SERVER
+app.listen(port, () => {
+    console.log(`The application started successfully on ${port}`);
+})
